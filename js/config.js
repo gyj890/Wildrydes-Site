0cce932dc8852dccea9b49c37d6633823559ef1c
Anushka Gupta <94749693+gyj890@users.noreply.github.com>
2025-11-15 17:07:09 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,11 +1,12 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_ZOrOv6ynD', // 
-        userPoolClientId: '382l312epapa7t0mp4nrj61286', // 
+        userPoolClientId: '7vl913nlr58o829qqa93l6d7n3', // 
         region: 'us-east-1' // 
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
